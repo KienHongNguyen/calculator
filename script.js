07570efe851c44f2d6f86fb14ca96fcdebd09ccd
KienHongNguyen <hongkien66@gmail.com>
2024-04-30 15:26:04 +1000

    halfway

--- script.js
+++ script.js
@@ -1,59 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const numberButtons = document.querySelectorAll('.number');
-    const functionButtons = document.querySelectorAll('.functionButton button');
-    const equalButton = document.getElementById('equalButton');
-    const clearButton = document.getElementById('clearButton');
-    const display = document.getElementById('display');
-    
-    let currentOperation = null;
-    let firstOperand = null;
-    let secondOperand = null;
-    let shouldResetScreen = false;
+// CALCULATOR PROGRAM
 
-    numberButtons.forEach(button => {
-        button.addEventListener('click', () => appendNumber(button.value));
-    })
+const display = document.getElementById("display");
 
-})
+function appendToDisplay(input) {
+    display.value += input;
+}
 
+function clearDisplay() {
+    display.value = "";
+}
 
-operate (10, "+", 12);
-operate (10, "-", 12);
-operate (10, "*", 12);
-operate (10, "/", 12);
-
-
-// Operate function
-function operate (num1, operator, num2) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
-        default:
-            return "Invalid operator";
+function calculate() {
+    try {
+        display.value = eval(display.value);
     }
+    catch(error) {
+        display.value = "Error";
+    }
+    
 }
-
-// Basic Mathematic Functions
-function add (num1, num2) {
-    console.log(num1+num2);
-}
-
-function subtract (num1, num2) {
-    console.log(num1-num2);
-}
-
-function multiply (num1, num2) {
-    console.log(num1*num2);
-}
-
-function divide (num1, num2) {
-    console.log(num1/num2);
-}
-
-
